f8630ae3fd09a08475e31f98a442de4b08bd9dc2
D_U <divya.bhosle@lntinfotech.com>
2019-11-14 12:24:29 +0530

    hello spring mvc

---
+++ SpringMVC_App/src/com/lti/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.lti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping(value="/hello" ,method=RequestMethod.GET)
+	public ModelAndView hello()
+	{
+		ModelAndView model=new ModelAndView("hello");
+		model.addObject("message", "Hello from spring mvc app");
+		return model;
+	}
+
+}
